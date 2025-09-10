cc99877f6c6be89a07434e39319b427954a499b8
Juan Luis Quijivix <jlqm@yahoo.com>
2025-09-09 18:16:50 -0600

    Added project structure

---
+++ CollegeFootball/CollegeFootball.Domain/Entities/TeamScore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollegeFootball.Domain.Entities
+{
+    public class TeamScore
+    {
+        public int Id { get; set; }
+        public int Rank { get; set; }
+        public string TeamName { get; set; } = string.Empty;
+        public string MascotName { get; set; } = string.Empty;
+        public DateTime? LastWinDate { get; set; }
+        public float? WinningPercentage { get; set; }
+        public int? TotalLosses { get; set; }
+        public int? TotalTies { get; set; }
+        public int? TotalWins { get; set; }
+    }
+}
